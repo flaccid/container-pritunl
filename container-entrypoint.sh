0f0cc5b8633a538a787f81fd01d45e352681220f
Chris Fordham <chris@fordham.id.au>
2023-06-18 18:11:23 +1000

    Take a different approach to the config volume.

--- container-entrypoint.sh
+++ container-entrypoint.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+if [ -e /opt/pritunl/etc/pritunl.conf ]; then 
+  echo "use /opt/pritunl/etc/pritunl.conf"
+  cp -vf /opt/pritunl/etc/pritunl.conf /etc/pritunl.conf
+fi
+
 if [ ! -z "$PRITUNL_MONGODB_URI" ]; then
-  echo "set mongodb uri to $PRITUNL_MONGODB_URI"
+  echo "set mongodb uri to [redacted]"
   pritunl set-mongodb "$PRITUNL_MONGODB_URI"
 fi
 
